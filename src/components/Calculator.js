425a96bee3fd2554562e5b1b3f0ae05570ea31ac
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-03-29 15:59:08 +0200

    Create event listener for buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,11 +2,19 @@ import './calculator.css';
 import Screen from './Screen';
 import Keyboard from './Keyboard';
 
-const Calculator = () => (
-  <div className="calculator-container">
-    <Screen />
-    <Keyboard />
-  </div>
-);
+// import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const handleButtonClick = (e) => {
+    const buttonName = e.target.innerText;
+    console.log(buttonName);
+  };
+  return (
+    <div className="calculator-container">
+      <Screen />
+      <Keyboard onClick={handleButtonClick} />
+    </div>
+  );
+};
 
 export default Calculator;
